5b68d7796d16030e4b11ce0d111e1be16a175e21
Adrian Chen <adrianchen@rgnt2-122-165-dhcp.int.colorado.edu>
2015-03-09 16:59:23 -0600

    completed milestone 2

--- public/scripts/tutorial8.js
+++ public/scripts/tutorial8.js
@@ -1,4 +1,4 @@
 var data = [
   {author: "Pete Hunt", text: "This is one comment"},
-  {author: "Jordan Walke", text: "This is *another* comment"}
+  {author: "Jordan Walke", text: "This is *yet another* comment"}
 ];
