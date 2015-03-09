e970e77cbcd1e8b0809d766d0e7f2ae7484bd5fa
Ryan Milvenan <ryanmilvenan@gmail.com>
2015-03-09 16:57:03 -0600

    completed milestone 1

--- public/scripts/example.js
+++ public/scripts/example.js
@@ -1,13 +1,62 @@
+var converter = new Showdown.converter();
+var data = [
+  {author: "Pete Hunt", text: "This is one comment"},
+  {author: "Jordan Walke", text: "This is *another* comment"}
+];
+
+var Comment = React.createClass({
+  render: function() {
+      var rawMarkup = converter.makeHtml(this.props.children.toString());
+      return (
+            <div className="comment">
+              <h2 className="commentAuthor">
+                {this.props.author}
+              </h2>
+              <span dangerouslySetInnerHTML={{__html: rawMarkup}} />
+            </div>
+          );
+    }
+});
+
+var CommentList = React.createClass({
+  render: function() {
+    var commentNodes = this.props.data.map(function (comment) {
+          return (
+                  <Comment author={comment.author}>
+                    {comment.text}
+                  </Comment>
+                );
+    });
+    return (
+          <div className="commentList">
+            {commentNodes}
+          </div>
+        );    
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+      return (
+            <div className="commentForm">
+              Hello, world! I am a CommentForm.
+            </div>
+          );
+    }
+});
+
 var CommentBox = React.createClass({
   render: function() {
     return (
       <div className="commentBox">
-        Hello, world! I am a CommentBox.
+        <h1>Comments</h1>
+        <CommentList data={this.props.data} />
+        <CommentForm />
       </div>
     );
   }
 });
 React.render(
-  <CommentBox />,
+  <CommentBox data={data} />,
   document.getElementById('content')
 )
