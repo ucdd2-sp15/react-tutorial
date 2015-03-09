9f6bc3b79af8ac282bd013d64f258c17354547c9
dawsonbotsford <dawsonbotsford@gmail.com>
2015-03-09 17:31:15 -0600

    completed milestone 2

--- public/scripts/example.js
+++ public/scripts/example.js
@@ -1,3 +1,7 @@
+var data = [
+  {author: "Pete Hunt", text: "This is one comment"},
+  {author: "Jordan Walke", text: "This is *another* comment"}
+];
 var converter = new Showdown.converter();
 var Comment = React.createClass({
   render: function() {
@@ -14,11 +18,17 @@ var Comment = React.createClass({
 })
 var CommentList = React.createClass({
   render: function() {
+    var commentNodes = this.props.data.map( function (comment) {
+      return (
+        <Comment author= {comment.author}>
+          {comment.text}
+        </Comment>
+      )
+    })
     return (
-    <div className = "CommentList">
-      <Comment author="Dawson">Comment number 1</Comment>
-      <Comment author="Joe">Comment number 2</Comment>
-    </div>
+      <div className = "commentList">
+        {commentNodes}
+      </div>
     )
   }
 })
@@ -32,17 +42,39 @@ var CommentForm = React.createClass({
   }
 })
 var CommentBox = React.createClass({
+  getInitialState: function() {
+    return {data: []};
+  },
+  loadCommentsFromServer: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    this.loadCommentsFromServer();
+    setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+  },
   render: function() {
     return (
       <div className="CommentBox">
         <h1> Comments </h1>
-        <CommentList />
+        <CommentList data = {this.state.data}/>
         <CommentForm />
       </div>
     );
   }
 });
 React.render(
-  <CommentBox />,
+  <CommentBox url="comments.json" pollInterval={2000}/>,
   document.getElementById('content')
 )
