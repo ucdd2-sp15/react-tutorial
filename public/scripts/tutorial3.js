75c68a87a7fac26c4a924b705d0975865140a87f
Adrian Chen <adrianchen@rgnt2-122-165-dhcp.int.colorado.edu>
2015-03-09 17:08:04 -0600

    completed milestone 4

--- public/scripts/tutorial3.js
+++ public/scripts/tutorial3.js
@@ -41,7 +41,7 @@ var CommentBox = React.createClass({
       <div className="commentBox">
         <h1>Comments</h1>
         <CommentList data={this.state.data} />
-        <CommentForm />
+        <CommentForm onCommentSubmit={this.handleCommentSubmit} />
       </div>
     );
   }
